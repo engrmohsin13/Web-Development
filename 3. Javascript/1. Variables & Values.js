e4b33af8c59a79b75429d22645a5a2ac0aa88615
engrmohsin13 <mohsintheengineer@gmail.com>
2025-04-14 13:55:45 +0500

    source file

---
+++ 3. Javascript/1. Variables & Values.js
@@ -0,0 +1,10 @@
+// Comments
+/*
+    Multiple line Comments
+*/
+
+var name = "Mohsin";
+let age = 22;
+const gender = "Male";
+
+console.log(`Name : ${name} || Age : ${age} || Gender : ${gender}`);
